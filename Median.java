dba665b7b97c74aeb63fbd207c249b129c5b69bd
IlanB94 <ilan1994@gmail.com>
2022-06-29 16:14:45 +0300

    Find median in two Arrays

---
+++ Median.java
@@ -0,0 +1,106 @@
+
+public class Median {
+	public static void main(String[] args) {
+		int [] nums1 = {1,2};
+		int [] nums2 = {3,4};
+		System.out.println(findMedianSortedArrays(nums1,nums2));
+
+	}
+	public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
+		int length = nums1.length + nums2.length;
+		int[] arr = new int[length];
+        MergeSort ob = new MergeSort();
+        int pos = 0;
+        for (int element : nums1) {
+            arr[pos] = element;
+            pos++;
+        }
+
+        for (int element : nums2) {
+            arr[pos] = element;
+            pos++;
+        }
+        
+        
+		ob.sort(arr, 0, arr.length-1);
+		if (arr.length % 2 != 0) {
+			return arr[arr.length / 2];
+		} else {
+			double left = arr[arr.length / 2-1];
+			System.out.println(left);
+			double right = arr[(arr.length / 2)];
+			System.out.println(right);
+			double avg = (left + right) / 2;
+			return avg;
+		} 
+    }
+}
+
+class MergeSort {
+	// Merges two subarrays of arr[].
+	// First subarray is arr[l..m]
+	// Second subarray is arr[m+1..r]
+	public void merge(int arr[], int l, int m, int r) {
+		// Find sizes of two subarrays to be merged
+		int n1 = m - l + 1;
+		int n2 = r - m;
+
+		/* Create temp arrays */
+		int L[] = new int[n1];
+		int R[] = new int[n2];
+
+		/* Copy data to temp arrays */
+		for (int i = 0; i < n1; ++i)
+			L[i] = arr[l + i];
+		for (int j = 0; j < n2; ++j)
+			R[j] = arr[m + 1 + j];
+
+		/* Merge the temp arrays */
+
+		// Initial indexes of first and second subarrays
+		int i = 0, j = 0;
+
+		// Initial index of merged subarray array
+		int k = l;
+		while (i < n1 && j < n2) {
+			if (L[i] <= R[j]) {
+				arr[k] = L[i];
+				i++;
+			} else {
+				arr[k] = R[j];
+				j++;
+			}
+			k++;
+		}
+
+		/* Copy remaining elements of L[] if any */
+		while (i < n1) {
+			arr[k] = L[i];
+			i++;
+			k++;
+		}
+
+		/* Copy remaining elements of R[] if any */
+		while (j < n2) {
+			arr[k] = R[j];
+			j++;
+			k++;
+		}
+	}
+
+	// Main function that sorts arr[l..r] using
+	// merge()
+	public void sort(int arr[], int l, int r) {
+		if (l < r) {
+			// Find the middle point
+			int m = l + (r - l) / 2;
+
+			// Sort first and second halves
+			sort(arr, l, m);
+			sort(arr, m + 1, r);
+
+			// Merge the sorted halves
+			merge(arr, l, m, r);
+		}
+	}
+}
