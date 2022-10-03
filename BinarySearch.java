2dffdcb34c335e022c00131666c725c91ab47db9
IlanB94 <ilan1994@gmail.com>
2022-10-03 11:41:53 +0300

    binary search

--- BinarySearch.java
+++ BinarySearch.java
@@ -1,24 +1,24 @@
 class BinarySearch {
 
 	public int search(int[] nums, int target) {
-        int mid = nums.length/2;
-        
-        if(target > nums[nums.length-1]) {
-        	return -1;
-        }else if(target < nums[mid]){
-        	for(int i =0; i < nums.length/2;i++) {
-        		if(nums[i] == target) {
-        			return i;
-        		}
-        	}
-        }else {
-        	for(int i = nums.length/2; i < nums.length;i++) {
-        		if(nums[i] == target) {
-        			return i;
-        		}
-        	}
-        }
-        return -1;
-    }
+		int mid = nums.length / 2;
+
+		if (target > nums[nums.length - 1]) {
+			return -1;
+		} else if (target < nums[mid]) {
+			for (int i = 0; i < nums.length / 2; i++) {
+				if (nums[i] == target) {
+					return i;
+				}
+			}
+		} else {
+			for (int i = nums.length / 2; i < nums.length; i++) {
+				if (nums[i] == target) {
+					return i;
+				}
+			}
+		}
+		return -1;
+	}
 
 }
