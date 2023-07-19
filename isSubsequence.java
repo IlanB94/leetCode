546267caee7f85912101257499d67292554b606c
IlanB94 <71595766+IlanB94@users.noreply.github.com>
2023-07-19 12:39:32 +0300

    Create isSubsequence.java
    Add isSubsequence solution

---
+++ isSubsequence.java
@@ -0,0 +1,16 @@
+ public boolean isSubsequence(String s, String t) {
+        int i = 0;
+		int j = 0;
+		while (i < s.length() && j != t.length()) {
+			if(s.charAt(i) == t.charAt(j)) {
+				i++;
+				j++;
+			}else {
+				j++;
+			}
+		}
+		if(i == s.length()) {
+			return true;
+		}
+		return false;
+    }
