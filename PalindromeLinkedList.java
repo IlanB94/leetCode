8e361d91014209247324b1076aabf3fbbf55d105
IlanB94 <71595766+IlanB94@users.noreply.github.com>
2023-07-25 11:39:18 +0300

    Update PalindromeLinkedList.java

--- PalindromeLinkedList.java
+++ PalindromeLinkedList.java
@@ -3,20 +3,6 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class PalindromeLinkedList {
-
-	public static void main(String[] args) {
-		ListNode l1 = new ListNode(1);
-		ListNode l2 = new ListNode(2);
-		ListNode l3 = new ListNode(2);
-		ListNode l4 = new ListNode(1);
-		
-		l1.next = l2;
-		l2.next=l3;
-		l3.next=l4;
-		System.out.println(isPalindrome(l1));
-
-	}
-
 	public static boolean isPalindrome(ListNode head) {
 		ListNode current = head;
 		Stack <Integer> stack = new Stack<Integer>();
