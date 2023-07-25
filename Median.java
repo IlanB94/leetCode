712b14b8d148847b46903dd7aadd2e4fbcdc5fd8
IlanB94 <71595766+IlanB94@users.noreply.github.com>
2023-07-25 11:38:47 +0300

    Update Median.java

--- Median.java
+++ Median.java
@@ -1,11 +1,6 @@
 
 public class Median {
-	public static void main(String[] args) {
-		int [] nums1 = {1,2};
-		int [] nums2 = {3,4};
-		System.out.println(findMedianSortedArrays(nums1,nums2));
-
-	}
+	
 	public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
 		int length = nums1.length + nums2.length;
 		int[] arr = new int[length];
