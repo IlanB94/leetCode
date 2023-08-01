d493c74f2735c6a2d9da729a53b6d9fa695e8b9b
IlanB94 <71595766+IlanB94@users.noreply.github.com>
2023-08-01 12:08:08 +0300

    Create isPowerOfFour.java

---
+++ isPowerOfFour.java
@@ -0,0 +1,15 @@
+ public boolean isPowerOfFour(int n) {
+       if(n == 0) {
+			return false;
+		}
+		if(n == 1) {
+			return true;
+		}
+		if(n % 4 != 0){
+			return false;
+			
+		}
+		else {
+			return isPowerOfFour(n / 4);
+		}
+    }
